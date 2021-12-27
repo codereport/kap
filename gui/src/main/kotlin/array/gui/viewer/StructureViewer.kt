ecc021b62720cc9ec6957118acdfc0c112680755
Elias Mårtenson <elias@dhsdevelopments.com>
2021-12-27 23:31:34 +0800

    New structure viewer
    The new version is more generic and will be better at rendering complex expressions including long trains

--- gui/src/main/kotlin/array/gui/viewer/StructureViewer.kt
+++ gui/src/main/kotlin/array/gui/viewer/StructureViewer.kt
@@ -11,12 +11,10 @@ import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Pane
-import javafx.scene.layout.Region
 import javafx.scene.layout.VBox
 import javafx.scene.shape.Line
 import javafx.stage.Stage
-import java.lang.Double.max
-
+import kotlin.math.max
 
 class StructureViewer {
     lateinit var graphContentPane: Pane
@@ -24,27 +22,29 @@ class StructureViewer {
     lateinit var expressionField: TextField
     lateinit var borderPane: BorderPane
 
-    var rootNode: KNode? = null
-
     fun showClicked(@Suppress("UNUSED_PARAMETER") actionEvent: ActionEvent) {
         parseExpression(expressionField.text)
     }
 
     private fun parseExpression(text: String) {
         val instr = client.engine.parse(StringSourceLocation(text))
-        val node = createGraph(instr)
-        rootNode = node
-        graphContentPane.applyCss()
-        graphContentPane.layout()
-        node.computePosition(0.0, 0.0)
-        val bounds = node.bounds()
-        graphContentPane.setPrefSize(bounds.width, bounds.height)
+        graphContentPane.children.clear()
+        val graph = createGraph(instr)
+        val root = graph.rootNode
+        if (root != null) {
+            graphContentPane.applyCss()
+            graphContentPane.layout()
+            graph.updateNodes()
+            val bounds = root.bounds()
+            graphContentPane.setPrefSize(bounds.width, bounds.height)
+        }
     }
 
-    private fun createGraph(instr: Instruction): KNode {
-        graphContentPane.children.clear()
-        val node = GraphNodeRegistry.buildNode(GraphNodeRegistry.GraphNodeContext(graphContentPane), instr)
-        return node
+    private fun createGraph(instr: Instruction): Graph {
+        val graph = Graph(graphContentPane)
+        val node = makeNodeFromInstr(graph, instr)
+        graph.rootNode = node
+        return graph
     }
 
     companion object {
@@ -63,6 +63,32 @@ class StructureViewer {
     }
 }
 
+class LabelledContainer(labelText: String, node: Node) : VBox() {
+    init {
+        styleClass.add("labelled-container")
+        children.add(Label(labelText).apply { styleClass.add("labelled-container-title") })
+        children.add(node)
+    }
+}
+
+class Graph(val pane: Pane) {
+    val nodes = ArrayList<KNode>()
+    val links = ArrayList<KNodeLink>()
+    var rootNode: KNode? = null
+
+    fun updateNodes() {
+        val n = rootNode
+        if (n != null) {
+            n.computePosition(0.0, 0.0)
+            links.forEach { link ->
+                val (x1, y1) = link.upValue.downPos()
+                val (x2, y2) = link.downValue.upPos()
+                pane.children.add(Line(x1, y1, x2, y2))
+            }
+        }
+    }
+}
+
 class CreateGraphException(message: String) : Exception(message)
 
 data class BoundsDimensions(val width: Double, val height: Double)
@@ -71,125 +97,38 @@ data class Coord(val x: Double, val y: Double)
 private const val NODE_SPACING_HORIZ = 50.0
 private const val NODE_SPACING_VERT = 50.0
 
-abstract class KNode {
+abstract class KNode(val graph: Graph) {
     abstract fun bounds(): BoundsDimensions
     abstract fun computePosition(x: Double, y: Double)
-    abstract fun connectPosition(): Coord
+    abstract fun downPos(): Coord
+    abstract fun upPos(): Coord
 }
 
-object GraphNodeRegistry {
-    fun <T : Instruction> buildNode(context: GraphNodeContext, instr: T): KNode {
-        return when (instr) {
-            is FunctionCall1Arg -> makeFunctionCall1ArgNode(context, instr)
-            is FunctionCall2Arg -> FunctionCall2ArgGraphNode(context, instr.fn, instr.leftArgs, instr.rightArgs)
-            is LiteralInteger -> LiteralIntegerGraphNode(context, instr)
-            is LiteralDouble -> LiteralDoubleGraphNode(context, instr)
-            is LiteralComplex -> LiteralComplexGraphNode(context, instr)
-            is VariableRef -> VariableRefGraphNode(context, instr)
-            is Literal1DArray -> Literal1DArrayGraphNode(context, instr)
-            else -> throw CreateGraphException("Unable to render instruction: ${instr}")
-        }
-    }
-
-    private fun makeFunctionCall1ArgNode(context: GraphNodeContext, instr: FunctionCall1Arg): KNode {
-        return when (val fn = instr.fn) {
-            is FunctionCallChain.Chain3 -> Chain3GraphNode1Arg(context, fn, instr.rightArgs)
-            else -> FunctionCall1ArgGraphNode(context, instr.fn, instr.rightArgs)
-        }
-    }
-
-    class GraphNodeContext(val panel: Pane)
+abstract class KNode1Arg(graph: Graph) : KNode(graph) {
+    abstract val rightArgLink: KNode
 }
 
-class LeftArgLine(x1: Double, y1: Double, x2: Double, y2: Double) : Line(x1, y1, x2, y2) {
-    init {
-        styleClass.add("left-arg-line")
+abstract class KNode2Arg(graph: Graph, val leftArgLink: KNode, val rightArgLink: KNode) : KNode(graph)
+
+abstract class KNodeLink(val upValue: KNode, val downValue: KNode)
+class LeftArgLink(upValue: KNode, downValue: KNode) : KNodeLink(upValue, downValue)
+class RightArgLink(upValue: KNode, downValue: KNode) : KNodeLink(upValue, downValue)
+
+fun makeNodeFromInstr(graph: Graph, instr: Instruction): KNode {
+    val node = when (instr) {
+        is LiteralInteger -> LiteralIntegerGraphNode(graph, instr)
+        is FunctionCall2Arg -> makeFunctionCall2ArgGraphNodeFromInstruction(graph, instr.fn, instr.leftArgs, instr.rightArgs)
+        else -> throw CreateGraphException("Unsupported instruction type: ${instr}")
     }
+    graph.nodes.add(node)
+    return node
 }
 
-class RightArgLine(x1: Double, y1: Double, x2: Double, y2: Double) : Line(x1, y1, x2, y2) {
-    init {
-        styleClass.add("right-arg-line")
-    }
-}
-
-open class FunctionCall1ArgGraphNode(val context: GraphNodeRegistry.GraphNodeContext, fn: APLFunction, rightArgs: Instruction) : KNode() {
-    val label = LabelledContainer("Call1", Label(fnName(fn))).apply { styleClass.addAll("call1") }
-    val rightNode = GraphNodeRegistry.buildNode(context, rightArgs)
+class LiteralIntegerGraphNode(graph: Graph, instr: LiteralInteger) : KNode(graph) {
+    private val label = LabelledContainer("Integer", Label(instr.value.toString()))
 
     init {
-        context.panel.children.add(label)
-    }
-
-    override fun bounds(): BoundsDimensions {
-        val rBounds = rightNode.bounds()
-        val width = max(rBounds.width, label.width)
-        val height = rBounds.height + label.height + NODE_SPACING_VERT
-        return BoundsDimensions(width, height)
-    }
-
-    override fun computePosition(x: Double, y: Double) {
-        val bounds = bounds()
-        label.layoutX = x + (bounds.width - label.width) / 2
-        label.layoutY = y
-        rightNode.computePosition(x + (bounds.width - rightNode.bounds().width) / 2, y + label.height + NODE_SPACING_VERT)
-
-        val (rx, ry) = rightNode.connectPosition()
-        context.panel.children.add(RightArgLine(label.layoutX + label.width / 2, label.layoutY + label.height, rx, ry))
-    }
-
-    override fun connectPosition() = Coord(label.layoutX + label.width / 2, label.layoutY)
-}
-
-open class FunctionCall2ArgGraphNode(
-    val context: GraphNodeRegistry.GraphNodeContext,
-    fn: APLFunction,
-    leftArgs: Instruction,
-    rightArgs: Instruction
-) : KNode() {
-    val label = LabelledContainer("Call2", Label(fnName(fn))).apply { styleClass.addAll("call2") }
-    val leftNode = GraphNodeRegistry.buildNode(context, leftArgs)
-    val rightNode = GraphNodeRegistry.buildNode(context, rightArgs)
-
-    init {
-        context.panel.children.add(label)
-    }
-
-    override fun bounds(): BoundsDimensions {
-        val lBounds = leftNode.bounds()
-        val rBounds = rightNode.bounds()
-        val width = max(lBounds.width + rBounds.width + NODE_SPACING_HORIZ, label.width)
-        val height = max(lBounds.height, rBounds.height) + label.height + NODE_SPACING_VERT
-        return BoundsDimensions(width, height)
-    }
-
-    override fun computePosition(x: Double, y: Double) {
-        val lBounds = leftNode.bounds()
-        rightNode.bounds()
-
-        label.layoutX = x + (bounds().width - label.width) / 2
-        label.layoutY = y
-        val argYPos = y + label.height + NODE_SPACING_VERT
-        leftNode.computePosition(x, argYPos)
-        val rightNodeX = x + lBounds.width + NODE_SPACING_HORIZ
-        rightNode.computePosition(rightNodeX, argYPos)
-
-        val (lx, ly) = leftNode.connectPosition()
-        val line1 = LeftArgLine(label.layoutX + label.width / 2, label.layoutY + label.height, lx, ly)
-        context.panel.children.add(line1)
-
-        val (rx, ry) = rightNode.connectPosition()
-        val line2 = RightArgLine(label.layoutX + label.width / 2, label.layoutY + label.height, rx, ry)
-        context.panel.children.add(line2)
-    }
-
-    override fun connectPosition() = Coord(label.layoutX + label.width / 2, label.layoutY)
-}
-
-abstract class SimpleLabelNode(val context: GraphNodeRegistry.GraphNodeContext, val label: Region, style: String) : KNode() {
-    init {
-        label.styleClass.add(style)
-        context.panel.children.add(label)
+        graph.pane.children.add(label)
     }
 
     override fun bounds() = BoundsDimensions(label.width, label.height)
@@ -199,46 +138,12 @@ abstract class SimpleLabelNode(val context: GraphNodeRegistry.GraphNodeContext, 
         label.layoutY = y
     }
 
-    override fun connectPosition() = Coord(label.layoutX + label.width / 2, label.layoutY)
-}
-
-class LiteralIntegerGraphNode(context: GraphNodeRegistry.GraphNodeContext, instr: LiteralInteger) :
-    SimpleLabelNode(context, LabelledContainer("Integer", Label(instr.value.toString())), "number-label")
-
-class LiteralDoubleGraphNode(context: GraphNodeRegistry.GraphNodeContext, instr: LiteralDouble) :
-    SimpleLabelNode(context, LabelledContainer("Double", Label(instr.value.toString())), "number-label")
-
-class LiteralComplexGraphNode(context: GraphNodeRegistry.GraphNodeContext, instr: LiteralComplex) :
-    SimpleLabelNode(context, LabelledContainer("Complex", Label("${instr.value.real}J${instr.value.imaginary}")), "number-label")
-
-class VariableRefGraphNode(context: GraphNodeRegistry.GraphNodeContext, instr: VariableRef) :
-    SimpleLabelNode(context, LabelledContainer("Variable Lookup", Label(instr.name.nameWithNamespace())), "variable-ref-label")
-
-class Literal1DArrayGraphNode(context: GraphNodeRegistry.GraphNodeContext, instr: Literal1DArray) : KNode() {
-    override fun bounds(): BoundsDimensions {
-        TODO("not implemented")
+    override fun downPos(): Coord {
+        return Coord(label.layoutX + label.width / 2, label.layoutY + label.height)
     }
 
-    override fun computePosition(x: Double, y: Double) {
-        TODO("not implemented")
-    }
-
-    override fun connectPosition(): Coord {
-        TODO("not implemented")
-    }
-}
-
-class Chain3GraphNode1Arg(context: GraphNodeRegistry.GraphNodeContext, fn: FunctionCallChain.Chain3, rightArgs: Instruction) : KNode() {
-    override fun bounds(): BoundsDimensions {
-        TODO("not implemented")
-    }
-
-    override fun computePosition(x: Double, y: Double) {
-        TODO("not implemented")
-    }
-
-    override fun connectPosition(): Coord {
-        TODO("not implemented")
+    override fun upPos(): Coord {
+        return Coord(label.layoutX + label.width / 2, label.layoutY)
     }
 }
 
@@ -251,10 +156,125 @@ private fun fnName(fn: APLFunction): String {
     }
 }
 
-class LabelledContainer(labelText: String, node: Node) : VBox() {
-    init {
-        styleClass.add("labelled-container")
-        children.add(Label(labelText).apply { styleClass.add("labelled-container-title") })
-        children.add(node)
+fun makeFunctionCall2ArgGraphNodeFromInstruction(graph: Graph, fn: APLFunction, leftArgs: Instruction, rightArgs: Instruction): KNode {
+    val leftArgsNode = makeNodeFromInstr(graph, leftArgs)
+    val rightArgsNode = makeNodeFromInstr(graph, rightArgs)
+    val fnNode = makeFunctionCall2ArgGraphNodeFromFunction(fn, graph, leftArgsNode, rightArgsNode)
+    return ContainerGraphNode(graph, fnNode, leftArgsNode, rightArgsNode)
+}
+
+private fun makeFunctionCall2ArgGraphNodeFromFunction(
+    fn: APLFunction,
+    graph: Graph,
+    leftArgsNode: KNode,
+    rightArgsNode: KNode
+): KNode {
+    val fnNode = when (fn) {
+        is FunctionCallChain.Chain3 -> Chain3GraphNode(graph, fn, leftArgsNode, rightArgsNode)
+        else -> FunctionCall2ArgGraphNode(graph, fn, leftArgsNode, rightArgsNode)
     }
+    return fnNode
+}
+
+class ContainerGraphNode(
+    graph: Graph, val fnNode: KNode, vararg val subNodes: KNode
+) : KNode(graph) {
+
+    init {
+        assert(subNodes.isNotEmpty())
+    }
+
+    override fun bounds(): BoundsDimensions {
+        val fnNodeBounds = fnNode.bounds()
+        val subNodesBounds = subNodes.map(KNode::bounds)
+        val width = max(
+            fnNodeBounds.width,
+            subNodesBounds.map(BoundsDimensions::width).sum() + NODE_SPACING_HORIZ * (max(0, subNodesBounds.size - 1)))
+        val height = fnNodeBounds.height + NODE_SPACING_VERT + subNodesBounds.maxValueBy(BoundsDimensions::height)
+        return BoundsDimensions(width, height)
+    }
+
+    override fun computePosition(x: Double, y: Double) {
+        val b = bounds()
+        val fnBounds = fnNode.bounds()
+        fnNode.computePosition(x + (b.width - fnBounds.width) / 2, y)
+        val subNodesY = y + fnBounds.height + NODE_SPACING_VERT
+        val widths = subNodes.map { n -> n.bounds().width }
+        val subNodesWidth = widths.sum() + NODE_SPACING_HORIZ * max(0, widths.size - 1)
+        var nodeXPos = (b.width - subNodesWidth) / 2
+        subNodes.forEachIndexed { i, n ->
+            n.computePosition(x + nodeXPos, subNodesY)
+            nodeXPos += widths[i] + NODE_SPACING_HORIZ
+        }
+    }
+
+    override fun downPos(): Coord {
+        TODO("not implemented")
+    }
+
+    override fun upPos(): Coord {
+        return fnNode.upPos()
+    }
+}
+
+class FunctionCall2ArgGraphNode(
+    graph: Graph, fn: APLFunction, leftArgLink: KNode, rightArgLink: KNode
+) : KNode2Arg(graph, leftArgLink, rightArgLink) {
+
+    private val label = LabelledContainer("Call2", Label(fnName(fn)))
+
+    init {
+        graph.pane.children.add(label)
+        graph.links.add(LeftArgLink(this, leftArgLink))
+        graph.links.add(RightArgLink(this, rightArgLink))
+    }
+
+    override fun bounds() = BoundsDimensions(label.width, label.height)
+
+    override fun computePosition(x: Double, y: Double) {
+        label.layoutX = x
+        label.layoutY = y
+    }
+
+    override fun downPos(): Coord {
+        return Coord(label.layoutX + label.width / 2, label.layoutY + label.height)
+    }
+
+    override fun upPos(): Coord {
+        return Coord(label.layoutX + label.width / 2, label.layoutY)
+    }
+}
+
+class Chain3GraphNode(graph: Graph, fn: FunctionCallChain.Chain3, leftArgLink: KNode, rightArgLink: KNode) : KNode(graph) {
+    val leftNode = makeFunctionCall2ArgGraphNodeFromFunction(fn.fn0, graph, leftArgLink, rightArgLink)
+    val rightNode = makeFunctionCall2ArgGraphNodeFromFunction(fn.fn2, graph, leftArgLink, rightArgLink)
+    val middleNode = makeFunctionCall2ArgGraphNodeFromFunction(fn.fn1, graph, leftNode, rightNode)
+
+    override fun bounds(): BoundsDimensions {
+        val mBounds = middleNode.bounds()
+        val lBounds = leftNode.bounds()
+        val rBounds = rightNode.bounds()
+        val width = max(mBounds.width, lBounds.width + rBounds.width + NODE_SPACING_HORIZ)
+        val height = mBounds.height + NODE_SPACING_VERT + max(lBounds.height, rBounds.height)
+        return BoundsDimensions(width, height)
+    }
+
+    override fun computePosition(x: Double, y: Double) {
+        val b = bounds()
+        val mBounds = middleNode.bounds()
+        val lBounds = leftNode.bounds()
+        val rBounds = rightNode.bounds()
+        val bottomWidth = lBounds.width + rBounds.width + NODE_SPACING_HORIZ
+        middleNode.computePosition(x + (b.width - mBounds.width) / 2, y)
+        val yOffset = mBounds.height + NODE_SPACING_VERT
+        val lx = x + (b.width - bottomWidth) / 2
+        leftNode.computePosition(lx, yOffset)
+        rightNode.computePosition(lx + lBounds.width + NODE_SPACING_HORIZ, yOffset)
+    }
+
+    override fun downPos(): Coord {
+        TODO("not implemented")
+    }
+
+    override fun upPos() = leftNode.upPos()
 }
