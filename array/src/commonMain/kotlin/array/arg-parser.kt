a6b04b5a7da7ccb954529f2a7b32dc683bd44830
Elias Mårtenson <elias@dhsdevelopments.com>
2023-02-04 14:33:02 +0800

    Improvements to ArgParser
    Includes support for short options

--- array/src/commonMain/kotlin/array/arg-parser.kt
+++ array/src/commonMain/kotlin/array/arg-parser.kt
@@ -1,13 +1,16 @@
 package array.options
 
-class InvalidOption(name: String) : Exception("Invalid arg: ${name}")
+class InvalidOptionDefinition(message: String) : Exception(message)
+open class OptionException(message: String) : Exception(message)
+class InvalidOption(name: String) : OptionException("Invalid arg: ${name}")
+class OptionSyntaxException(message: String) : OptionException(message)
 
-class Option(val name: String, val requireArg: Boolean = false, val description: String? = null)
+class Option(val name: String, val requireArg: Boolean = false, val description: String? = null, val shortOption: String? = null)
 class OptionResult(val option: Option, val arg: String?)
 
 private val LONG_OPTION_ARG_PATTERN = "^--([a-zA-Z0-9-]+)=(.*)$".toRegex()
 private val LONG_OPTION_NO_ARG_PATTERN = "^--([a-zA-Z0-9-]+)$".toRegex()
-private val SHORT_OPTION_PATTERN = "^-[a-zA-Z0-9]$".toRegex()
+private val SHORT_OPTION_PATTERN = "^-([a-zA-Z0-9]+)$".toRegex()
 
 class ArgParser(vararg options: Option) {
     val definedOptions: Map<String, Option>
@@ -15,15 +18,65 @@ class ArgParser(vararg options: Option) {
     init {
         definedOptions = HashMap()
         options.forEach { option ->
+            if (definedOptions.containsKey(option.name)) {
+                throw InvalidOptionDefinition("Duplicated option name found: ${option.name}")
+            }
+            val s = option.shortOption
+            if (s != null) {
+                if (definedOptions.values.find { v -> v.shortOption == s } != null) {
+                    throw InvalidOptionDefinition("Duplicated short option found: ${option.name}")
+                }
+            }
             definedOptions[option.name] = option
         }
     }
 
     fun parse(args: Array<String>): Map<String, String?> {
         val parseResults = HashMap<String, String?>()
-        args.forEach { arg ->
-            val option = matchOption(arg)
-            parseResults[option.option.name] = option.arg
+        var i = 0
+        while (i < args.size) {
+            val arg = args[i++]
+            val argResult = LONG_OPTION_ARG_PATTERN.matchEntire(arg)
+            if (argResult != null) {
+                val option = lookup(arg, argResult.groups.get(1)!!.value, argResult.groups.get(2)!!.value)
+                parseResults[option.option.name] = option.arg
+            } else {
+                val longNoArgResult = LONG_OPTION_NO_ARG_PATTERN.matchEntire(arg)
+                if (longNoArgResult != null) {
+                    val option = lookup(arg, longNoArgResult.groups.get(1)!!.value, null)
+                    parseResults[option.option.name] = option.arg
+                } else {
+                    val shortResult = SHORT_OPTION_PATTERN.matchEntire(arg)
+                    if (shortResult != null) {
+                        val shortArgString = shortResult.groups.get(1)!!.value
+                        shortArgString.forEach { ch ->
+                            val option = lookupSingleChar(ch.toString())
+                            if (option.requireArg) {
+                                if (i >= args.size) {
+                                    throw OptionSyntaxException("Missing argument to \"${option.shortOption}\" parameter")
+                                }
+                                val parameter = args[i++]
+                                parseResults[option.name] = parameter
+                            } else {
+                                parseResults[option.name] = null
+                            }
+                        }
+                    } else {
+                        throw InvalidOption(arg)
+                    }
+                }
+            }
+//            val option = if (argResult != null) {
+//                lookup(arg, argResult.groups.get(1)!!.value, argResult.groups.get(2)!!.value)
+//            } else {
+//                val longNoArgResult = LONG_OPTION_NO_ARG_PATTERN.matchEntire(arg)
+//                if (longNoArgResult != null) {
+//                    lookup(arg, longNoArgResult.groups.get(1)!!.value, null)
+//                } else {
+//                    throw InvalidOption(arg)
+//                }
+//            }
+//            parseResults[option.option.name] = option.arg
         }
         return parseResults
     }
@@ -44,25 +97,20 @@ class ArgParser(vararg options: Option) {
         }
     }
 
-    private fun matchOption(arg: String): OptionResult {
-        val argResult = LONG_OPTION_ARG_PATTERN.matchEntire(arg)
-        return if (argResult != null) {
-            lookup(arg, argResult.groups.get(1)!!.value, argResult.groups.get(2)!!.value)
-        } else {
-            val longNoArgResult = LONG_OPTION_NO_ARG_PATTERN.matchEntire(arg)
-            if (longNoArgResult != null) {
-                lookup(arg, longNoArgResult.groups.get(1)!!.value, null)
-            } else {
-                throw InvalidOption(arg)
-            }
-        }
-    }
-
     private fun lookup(originalArg: String, name: String, arg: String?): OptionResult {
         val option = definedOptions[name] ?: throw InvalidOption(originalArg)
         if ((option.requireArg && arg == null) || (!option.requireArg && arg != null)) {
             throw InvalidOption(originalArg)
         }
         return OptionResult(option, arg)
+    }
+
+    private fun lookupSingleChar(s: String): Option {
+        definedOptions.values.forEach { option ->
+            if (option.shortOption != null && option.shortOption == s) {
+                return option
+            }
+        }
+        throw InvalidOption(s)
     }
 }
--- array/src/commonTest/kotlin/array/options/ArgParserTest.kt
+++ array/src/commonTest/kotlin/array/options/ArgParserTest.kt
@@ -3,6 +3,7 @@ package array.options
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 class ArgParserTest {
     @Test
@@ -11,6 +12,7 @@ class ArgParserTest {
         val result = parser.parse(arrayOf("--foo=a", "--bar"))
         assertEquals(2, result.size)
         assertEquals("a", result["foo"])
+        assertTrue(result.containsKey("bar"))
         assertEquals(null, result["bar"])
     }
 
@@ -53,5 +55,123 @@ class ArgParserTest {
         assertFailsWith<InvalidOption> {
             ArgParser(Option("foo", true), Option("bar", false)).parse(arrayOf(" --foo=a"))
         }
+    }
+
+    @Test
+    fun shortOptionOneArg() {
+        val result = ArgParser(Option("foo", shortOption = "f")).parse(arrayOf("-f"))
+        assertEquals(1, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+    }
+
+    @Test
+    fun shortOptionTwoArg() {
+        val result = ArgParser(Option("foo", shortOption = "f"), Option("xyz", shortOption = "x")).parse(arrayOf("-fx"))
+        assertEquals(2, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+        assertTrue(result.containsKey("xyz"))
+        assertEquals(null, result["xyz"])
+    }
+
+    @Test
+    fun shortOptionTwoArgMultiEntry() {
+        val result = ArgParser(Option("foo", shortOption = "f"), Option("xyz", shortOption = "x")).parse(arrayOf("-f", "-x"))
+        assertEquals(2, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+        assertTrue(result.containsKey("xyz"))
+        assertEquals(null, result["xyz"])
+    }
+
+    @Test
+    fun shortOptionTwoArgMixLong() {
+        val result = ArgParser(Option("foo", shortOption = "f"), Option("xyz", shortOption = "x")).parse(arrayOf("-f", "--xyz"))
+        assertEquals(2, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+        assertTrue(result.containsKey("xyz"))
+        assertEquals(null, result["xyz"])
+    }
+
+    @Test
+    fun shortOptionTwoArgMixLongWithArg() {
+        val result = ArgParser(
+            Option("foo", shortOption = "f"),
+            Option("xyz", requireArg = true, shortOption = "x"))
+            .parse(arrayOf("-f", "--xyz=a"))
+        assertEquals(2, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+        assertTrue(result.containsKey("xyz"))
+        assertEquals("a", result["xyz"])
+    }
+
+    @Test
+    fun shortOptionArgWithParam() {
+        val result = ArgParser(
+            Option("foo", shortOption = "f"),
+            Option("xyz", requireArg = true, shortOption = "x"))
+            .parse(arrayOf("-x", "a"))
+        assertEquals(1, result.size)
+        assertTrue(result.containsKey("xyz"))
+        assertEquals("a", result["xyz"])
+    }
+
+    @Test
+    fun invalidShortArg() {
+        assertFailsWith<InvalidOption> {
+            ArgParser(Option("foo", shortOption = "f")).parse(arrayOf("-x"))
+        }
+    }
+
+    @Test
+    fun duplicateArgNames0() {
+        assertFailsWith<InvalidOptionDefinition> {
+            ArgParser(Option("foo", shortOption = "z"), Option("bar", shortOption = "z"))
+        }
+    }
+
+    @Test
+    fun duplicateArgNames1() {
+        assertFailsWith<InvalidOptionDefinition> {
+            ArgParser(Option("foo", shortOption = "a"), Option("foo", shortOption = "b"))
+        }
+    }
+
+    @Test
+    fun multipleCopiesOfSameArg0() {
+        val result = ArgParser(Option("foo", shortOption = "f"), Option("xyz", shortOption = "x")).parse(arrayOf("-f", "-f"))
+        assertEquals(1, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+    }
+
+    @Test
+    fun multipleCopiesOfSameArg1() {
+        val result = ArgParser(Option("foo", shortOption = "f"), Option("xyz", shortOption = "x")).parse(arrayOf("--foo", "--foo"))
+        assertEquals(1, result.size)
+        assertTrue(result.containsKey("foo"))
+        assertEquals(null, result["foo"])
+    }
+
+    @Test
+    fun multipleCopiesOfSameArg2() {
+        val result = ArgParser(
+            Option("foo", requireArg = true, shortOption = "f"),
+            Option("xyz", shortOption = "x"))
+            .parse(arrayOf("--foo=a", "--foo=b"))
+        assertEquals(1, result.size)
+        assertEquals("b", result["foo"])
+    }
+
+    @Test
+    fun multipleCopiesOfSameArg3() {
+        val result =
+            ArgParser(Option("foo", requireArg = true, shortOption = "f"), Option("xyz", shortOption = "x"))
+                .parse(arrayOf("-f", "a", "--foo=b"))
+        assertEquals(1, result.size)
+        assertEquals("b", result["foo"])
     }
 }
