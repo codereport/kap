f0dad6ee27cb421dcacaaa8edc2a2d48cbd23578
Elias Mårtenson <elias@dhsdevelopments.com>
2023-03-23 00:35:06 +0800

    Fix deployment script

--- scripts/copysite.sh
+++ scripts/copysite.sh
@@ -5,17 +5,17 @@ set -e
 asciidoctor -a toc ./website/src/orchid/resources/pages/kap-comparison.asciidoc
 asciidoctor -a toc ./website/src/orchid/resources/pages/tutorial.asciidoc
 
-scp -r /home/elias/prog/array_kotlin/website/src/orchid/resources/pages/kap-comparison.html \
-       /home/elias/prog/array_kotlin/website/src/orchid/resources/pages/tutorial.html \
-       /home/elias/prog/array_kotlin/website/src/orchid/resources/pages/diagrams \
+scp -r website/src/orchid/resources/pages/kap-comparison.html \
+       website/src/orchid/resources/pages/tutorial.html \
+       website/src/orchid/resources/pages/diagrams \
         root@matrix.dhsdevelopments.com:/var/www/kapdemo
 
 ./gradlew clientweb:browserDistribution
-scp -r /home/elias/prog/array_kotlin/clientweb/build/distributions/* \
-       /home/elias/prog/array_kotlin/array/standard-lib \
+scp -r clientweb/build/distributions/* \
+       array/standard-lib \
        root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb
 
 scripts/buildsite.sh
 
-scp -r /home/elias/prog/array_kotlin/clientweb2/build/distributions/* \
+scp -r clientweb2/build/distributions/* \
        root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb2
