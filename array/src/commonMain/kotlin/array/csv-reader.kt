60f54898c1bff36a7d3616a819361522c7a8503a
Elias Mårtenson <elias@dhsdevelopments.com>
2023-04-09 20:41:22 +0800

    Handle multiline fields in csv

--- array/src/commonMain/kotlin/array/csv-reader.kt
+++ array/src/commonMain/kotlin/array/csv-reader.kt
@@ -5,15 +5,7 @@ import array.*
 class CsvParseException(msg: String, val line: Int, val col: Int) : APLEvalException("Error at ${line + 1}:${col + 1}: $msg")
 
 fun readCsv(source: CharacterProvider): APLValue {
-    val rows = ArrayList<List<APLValue>>()
-    var lineNumber = 0
-    while (true) {
-        val line = source.nextLine() ?: break
-        val row = readRow(line, lineNumber++)
-        if (row != null) {
-            rows.add(row)
-        }
-    }
+    val rows = readRows(source)
 
     if (rows.isEmpty()) {
         return APLArrayImpl.make(dimensionsOfSize(0, 0)) {
@@ -34,86 +26,92 @@ fun readCsv(source: CharacterProvider): APLValue {
     }
 }
 
-private fun readRow(line: String, lineNumber: Int): List<APLValue>? {
-    val fields = ArrayList<APLValue>()
-    var pos = 0
+private fun readRows(source: CharacterProvider): List<List<APLValue>> {
+    val rows = ArrayList<List<APLValue>>()
+    var lineNumber = 0
+    while (true) {
+        var line = source.nextLine() ?: break
+        val currLineNumber = lineNumber++
+        val fields = ArrayList<APLValue>()
+        var pos = 0
 
-    fun atEol() = pos >= line.length
+        fun atEol() = pos >= line.length
 
-    fun skipWhitespace() {
-        while (!atEol() && line[pos].isWhitespace()) {
-            pos++
-        }
-    }
-
-    fun readQuotedField(): String {
-        val buf2 = StringBuilder()
-        loop@ while (true) {
-            if (atEol()) {
-                throwAPLException(CsvParseException("Unterminated string", lineNumber, pos))
+        fun skipWhitespace() {
+            while (!atEol() && line[pos].isWhitespace()) {
+                pos++
             }
-            when (val ch = line[pos++]) {
-                '\"' -> break@loop
-                '\\' -> {
-                    if (atEol()) {
-                        throwAPLException(CsvParseException("Unterminated string", lineNumber, pos))
-                    } else {
-                        buf2.append(line[pos++])
+        }
+
+        fun readQuotedField(): String {
+            val buf2 = StringBuilder()
+            loop@ while (true) {
+                if (atEol()) {
+                    line = source.nextLine() ?: throwAPLException(CsvParseException("End of file in the middle of string", currLineNumber, pos))
+                    pos = 0
+                    buf2.append("\n")
+                }
+                when (val ch = line[pos++]) {
+                    '\"' -> break@loop
+                    '\\' -> {
+                        if (atEol()) {
+                            throwAPLException(CsvParseException("Unterminated string", currLineNumber, pos))
+                        } else {
+                            buf2.append(line[pos++])
+                        }
+                    }
+                    else -> buf2.append(ch)
+                }
+            }
+            return buf2.toString()
+        }
+
+        fun readUnquotedField(initial: Char): String {
+            val buf = StringBuilder()
+            buf.append(initial)
+            var heldWhitespace = StringBuilder()
+            loop@ while (!atEol()) {
+                val ch = line[pos]
+                when {
+                    ch.isWhitespace() -> heldWhitespace.append(ch)
+                    ch == ',' -> break@loop
+                    else -> {
+                        buf.append(heldWhitespace)
+                        heldWhitespace = StringBuilder()
+                        buf.append(ch)
                     }
                 }
-                else -> buf2.append(ch)
+                pos++
             }
+            return buf.toString()
         }
-        return buf2.toString()
-    }
-
-    fun readUnquotedField(initial: Char): String {
-        val buf = StringBuilder()
-        buf.append(initial)
-        var heldWhitespace = StringBuilder()
-        loop@ while (!atEol()) {
-            val ch = line[pos]
-            when {
-                ch.isWhitespace() -> heldWhitespace.append(ch)
-                ch == ',' -> break@loop
-                else -> {
-                    buf.append(heldWhitespace)
-                    heldWhitespace = StringBuilder()
-                    buf.append(ch)
-                }
-            }
-            pos++
-        }
-        return buf.toString()
-    }
-
-    skipWhitespace()
-    if (atEol()) {
-        return null
-    }
-
-    while (true) {
-        skipWhitespace()
-        if (atEol()) break
-        val field = when (val ch = line[pos++]) {
-            '"' -> APLString.make(readQuotedField())
-            ',' -> {
-                pos--
-                APLString.make("")
-            }
-            else -> stringToAplValue(readUnquotedField(ch))
-        }
-        fields.add(field)
-        // At this point the next character must be either a comma or we're at the end of the line
         skipWhitespace()
         if (!atEol()) {
-            val ch2 = line[pos++]
-            if (ch2 != ',') {
-                throwAPLException(CsvParseException("Syntax error in CSV file", lineNumber, pos))
+            while (true) {
+                skipWhitespace()
+                if (atEol()) break
+                val field = when (val ch = line[pos++]) {
+                    '"' -> APLString.make(readQuotedField())
+                    ',' -> {
+                        pos--
+                        APLString.make("")
+                    }
+                    else -> stringToAplValue(readUnquotedField(ch))
+                }
+                fields.add(field)
+                // At this point the next character must be either a comma or we're at the end of the line
+                skipWhitespace()
+                if (!atEol()) {
+                    val ch2 = line[pos++]
+                    if (ch2 != ',') {
+                        throwAPLException(CsvParseException("Syntax error in CSV file", currLineNumber, pos))
+                    }
+                }
             }
+            rows.add(fields)
         }
     }
-    return fields
+    return rows
 }
 
 private val PATTERN_INTEGER = "^-?[0-9]+$".toRegex()
