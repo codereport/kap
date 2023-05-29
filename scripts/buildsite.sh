2fc8318e465205c5b6a6f714890315d8cc7ccea7
Elias Mårtenson <elias@dhsdevelopments.com>
2023-05-30 00:53:51 +0800

    Fixed some scripts to deal with the new output paths

--- scripts/buildsite.sh
+++ scripts/buildsite.sh
@@ -4,4 +4,4 @@ set -e
 
 ./gradlew clientweb2:webworkerBrowserDistribution
 ./gradlew clientweb2:jsBrowserDistribution
-cp -r array/standard-lib clientweb2/build/distributions
+#cp -r array/standard-lib clientweb2/build/dist/jsclient/productionExecutable/
--- scripts/copysite.sh
+++ scripts/copysite.sh
@@ -10,12 +10,16 @@ scp -r website/src/orchid/resources/pages/kap-comparison.html \
        website/src/orchid/resources/pages/diagrams \
         root@matrix.dhsdevelopments.com:/var/www/kapdemo
 
-./gradlew clientweb:browserDistribution
-scp -r clientweb/build/distributions/* \
-       array/standard-lib \
-       root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb
+#./gradlew clientweb:browserDistribution
+#scp -r clientweb/build/distributions/* \
+#       array/standard-lib \
+#       root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb
 
 scripts/buildsite.sh
 
-scp -r clientweb2/build/distributions/* \
+scp -r clientweb2/build/dist/jsclient/productionExecutable/* \
+       root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb2
+scp -r clientweb2/build/dist/webworker/productionExecutable/* \
+       root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb2
+scp -r array/standard-lib \
        root@matrix.dhsdevelopments.com:/var/www/kapdemo/clientweb2
